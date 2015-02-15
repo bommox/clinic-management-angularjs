f8dd906e6796d4a4d037f61d2829809680a7d466
Jorge Blom-Dahl Oliver <jorgeblom@mbp-jorge.local>
2015-02-15 21:17:26 +0100

    First Angular generated commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clinicaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the clinicaApp
+ */
+angular.module('clinicaApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clinicaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the clinicaApp
+ */
+angular.module('clinicaApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
